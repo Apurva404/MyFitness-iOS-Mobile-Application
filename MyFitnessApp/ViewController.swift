f584742bb479fe9482b6797ccc60497b0f8f0a52
Apurva Kumari <apurvajha90@gmail.com>
2017-10-11 18:37:50 -0700

    Initial Commit

---
+++ MyFitnessApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFitnessApp
+//
+//  Created by Apurva Kumari on 10/11/17.
+//  Copyright © 2017 Apurva Kumari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
